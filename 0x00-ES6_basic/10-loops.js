cf321956062f09cb93d99d953e5c61bb773710a0
root <imuranaishat38@gmail.com>
2024-08-05 19:48:43 +0000

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+)
+rt default function appendToEachArrayValue(array, appendString) {
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
+
+  return arrayEnd;
+}
