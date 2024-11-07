e20ceacf4599977656e607b40f33729ee0eac3cf
root <imuranaishat38@gmail.com>
2024-11-07 20:18:51 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Aishat <https://github.com/Imuranaishat>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
